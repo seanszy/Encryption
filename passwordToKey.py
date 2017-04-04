cb8bb92ec42d663a20b5466386d7d6aa7b19690b
Chris Aring <christopher.aring@students.olin.edu>
2017-04-04 15:01:38 -0400

    passwordToKey Init

---
+++ passwordToKey.py
@@ -0,0 +1,13 @@
+import random
+
+def passwordToKey(password):
+    password = "sadaskjdhaijshdkj"
+    sum_char = ""
+    for char in password:
+        char = ord(char) + 100
+        char = str(char)
+        sum_char += char
+
+    random.seed(sum_char)
+    key_short = random.randint(1000000, 10000000)
+    return(key_short)
