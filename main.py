1a217000cc14707a488dbfa7f8420b8365a91d77
Sean Szymanski <sean.szymanski@students.olin.edu>
2017-04-04 19:51:29 -0400

    Main with Functions

--- main.py
+++ main.py
@@ -9,35 +9,52 @@ import passwordToKey
 
 
 password = "yolos"
-long_key_password_int = passwordToKey.passwordToKey(password) #converts the user password to an integer
-long_key_password = binary.to_binary(long_key_password_int) #converts users password to a binary
 
-#randomly generate p and q
-p = random.randint(1000000, 10000000)
-q = random.randint(1000000, 10000000)
-print(" Origin P:", p, "\n", "Origin Q:", q)
+def create_Encypher(password):
+    long_key_password_int = passwordToKey.passwordToKey(password) #converts the user password to an integer
+    long_key_password = binary.to_binary(long_key_password_int) #converts users password to a binary
 
-#convert P and Q to binary
-long_key_one = p
-long_key_two = q
-long_key_one_binary = binary.to_binary(long_key_one)
-long_key_two_binary = binary.to_binary(long_key_two)
 
-#XOR P and Q
-Xored_p = XOR.XOR(long_key_one_binary, long_key_password)
-Xored_q = XOR.XOR(long_key_two_binary, long_key_password)
+    #randomly generate p and q
+    p = random.randint(1000000, 10000000)
+    q = random.randint(1000000, 10000000)
+    print(" Origin P:", p, "\n", "Origin Q:", q)
 
-#This is the encypher which we store on the computer
-print("Encypher:", Xored_p + "Z" + Xored_q)
+    #convert P and Q to binary
+    long_key_one = p
+    long_key_two = q
+    long_key_one_binary = binary.to_binary(long_key_one)
+    long_key_two_binary = binary.to_binary(long_key_two)
 
-#XOR the password again. Used to decrypt Encypher
-deXore_p = XOR.XOR(Xored_p, long_key_password)
-deXore_q = XOR.XOR(Xored_q, long_key_password)
+    #XOR P and Q
+    Xored_p = XOR.XOR(long_key_one_binary, long_key_password)
+    Xored_q = XOR.XOR(long_key_two_binary, long_key_password)
 
-#Convert from binary back to original int
-decoded_one = binary.from_binary(deXore_p)
-decoded_two = binary.from_binary(deXore_q)
-print(" Decode P:", decoded_one)
-print(" Decode Q:", decoded_two)
+    #This is the encypher which we store on the computer
+    #print("Encypher:", Xored_p + "Z" + Xored_q)
+    Encypher = Xored_p + "Z" + Xored_q
+    print("Encypher:", Encypher)
+    return Encypher
 
-#RSA_Encryption.run_rsa()
+def decode_Encypher(password, Encypher):
+    long_key_password_int = passwordToKey.passwordToKey(password) #converts the user password to an integer
+    long_key_password = binary.to_binary(long_key_password_int) #converts users password to a binary
+
+    index = Encypher.find("Z")
+    Xored_p_from_cypher = Encypher[0:index]
+    Xored_q_from_cypher = Encypher[index+1:]
+
+    #XOR the password again. Used to decrypt Encypher
+    deXore_p = XOR.XOR(Xored_p_from_cypher, long_key_password)
+    deXore_q = XOR.XOR(Xored_q_from_cypher, long_key_password)
+
+    #Convert from binary back to original int
+    decoded_one = binary.from_binary(deXore_p)
+    decoded_two = binary.from_binary(deXore_q)
+    print(" Decode P:", decoded_one)
+    print(" Decode Q:", decoded_two)
+
+    #RSA_Encryption.run_rsa()
+password = input("ENTER PASSWORD\n")
+Encypher = create_Encypher(password)
+decode_Encypher(password, Encypher)
