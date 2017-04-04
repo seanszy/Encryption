315c8313e404528f1a74022b48a65f2a80e90858
Sean Szymanski <sean.szymanski@students.olin.edu>
2017-04-04 17:56:43 -0400

    Implement Comments

--- XOR.py
+++ XOR.py
@@ -1,21 +1,31 @@
 def XOR(b1, b2):
+    """This function is used to conduct XOR logic. We use XOR as a way to
+    encrypt our private keys. This function runs XOR on two binary numbers that
+    are passed in"""
+    
     b1 = str(b1)
     b2 = str(b2)
+
+    #binary numbers can start with "0b", which throws an error. To prevent this
+    #those letters were removed if they existed
     if b1[0:2] == '0b':
         b1 = b1[2:]
     if b2[0:2] == '0b':
         b2 = b2[2:]
+
+    #converts strings into list of integers
     b1 = [int(x) for x in str(b1)]
     b2 = [int(x) for x in str(b2)]
     b3 = []
 
-    if len(b1) > len(b2):
+    #does XOR logic
+    if len(b1) > len(b2): #data will be lost if b1 > b2, so we print ERROR. This should never occur
         print("ERROR")
     else:
-        for i, bit in enumerate(b1):
-            if bit + b2[i] is 1:
+        for i, bit in enumerate(b1): #moves through each item in list
+            if bit + b2[i] is 1: #XOR is 1 if they add to 1
                 b3.append(1)
-            else:
+            else: #It is 0 if they do not
                 b3.append(0)
-    b3 = ''.join(map(str, b3))
+    b3 = ''.join(map(str, b3)) #combine them into a string
     return(b3)
--- main.py
+++ main.py
@@ -1,28 +1,39 @@
+"""This is the main function of the document...WRITE MORE WHEN IM NOT LAZY AF"""
+
+#Import all the other python files that we want to run
 import random
 import binary
 import XOR
 import passwordToKey
 
-password = "yolos"
-long_key_password_int = passwordToKey.passwordToKey(password)
-print(long_key_password_int)
-long_key_password = binary.to_binary(long_key_password_int)
 
+password = "yolos"
+long_key_password_int = passwordToKey.passwordToKey(password) #converts the user password to an integer
+long_key_password = binary.to_binary(long_key_password_int) #converts users password to a binary
+
+#randomly generate p and q
 p = random.randint(1000000, 10000000)
 q = random.randint(1000000, 10000000)
 print(" Origin P:", p, "\n", "Origin Q:", q)
 
+#convert P and Q to binary
 long_key_one = p
 long_key_two = q
 long_key_one_binary = binary.to_binary(long_key_one)
 long_key_two_binary = binary.to_binary(long_key_two)
+
+#XOR P and Q
 Xored_p = XOR.XOR(long_key_one_binary, long_key_password)
 Xored_q = XOR.XOR(long_key_two_binary, long_key_password)
 
+#This is the encypher which we store on the computer
 print("Encypher:", Xored_p + "Z" + Xored_q)
 
+#XOR the password again. Used to decrypt Encypher
 deXore_p = XOR.XOR(Xored_p, long_key_password)
 deXore_q = XOR.XOR(Xored_q, long_key_password)
+
+#Convert from binary back to original int
 decoded_one = binary.from_binary(deXore_p)
 decoded_two = binary.from_binary(deXore_q)
 print(" Decode P:", decoded_one)
