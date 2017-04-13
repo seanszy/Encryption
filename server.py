ee2564374ca1cee7a4433dde332051fb061d4e4b
Chris Aring <christopher.aring@students.olin.edu>
2017-04-13 18:39:14 -0400

    Fixed and Cleaned up files related to website. Also made better server.py file. Currently the server.py file does not work. It is not fully tested and has to be edited to work with main.

--- server.py
+++ server.py
@@ -1,95 +1,106 @@
-from flask import Flask, render_template, request, redirect
-import hashlib
-import uuid
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
 import csv
-import os.path
 import main
 
 app = Flask(__name__)
 
 exists = os.path.isfile("users.csv")
 
-@app.route('/')
+@app.route("/")
 def index():
     if not exists:
-        with open('users.csv', 'a') as csvfile:
-            fieldnames = ['username', 'password', 'key']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-    return render_template('index.html')
-
-
-@app.route('/loginreg', methods=['POST'])
-def pick():
-    if request.form['loginreg'] == 'Login':
+        with open('users.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    if not session.get('logged_in'):
         return render_template('login.html')
-    elif request.form['loginreg'] == 'Register':
-        return render_template('signup.html')
     else:
-        return redirect('/')
+        return render_template('user.html')
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    error = None
-    username = (request.form['username']).lower()
-    password = request.form['password']
-    if check_user_pass(username, password):
-        key = get_user_key(username, password)
-        a = main.decode_encypher(password, key)
-        print(a)
-        return render_template('user.html', user = username)
-    else:
-        error = "Username or password is incorrect. Please try again."
-        return render_template('login.html', error = error)
+    if request.form['submit'] == 'Login':
+        username = (request.form['username']).lower()
+        password = request.form['password']
+        if check_user_pass(username, password):
+            # key = get_user_key(username, password)
+            # a = main.decode_encypher(password, key)
+            session['logged_in'] = True
+            session['user'] = username
+            return render_template('/')
+        else:
+            flash('Wrong Password')
+    elif request.form['submit'] == 'Register':
+        return render_template('register.html')
+    return index()
 
 
-@app.route('/signup', methods=['POST'])
-def signup():
-    error = None
-    username = (request.form['username']).lower()
+@app.route('/register', methods=['POST'])
+def register():
+    username = (request.form['password'].lower())
     password = request.form['password']
     if not check_user(username):
-        key = main.create_encypher(password)
-        write(username, password, key)
-        return redirect('/')
+        # key = main.create_encypher(password)
+        # write(username, password, key)
+        return redirect('/login')
     else:
-        error = "Username is taken. Please try again."
-        return render_template('signup.html', error = error)
+        return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return index()
+
+
+@app.route('/user', methods=['POST'])
+def user():
+    if not session[logged_in]:
+        return render_template('/')
+    username = session['nickname']
+    with open('users.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        rows = [row for row in reader if row[0] == username]
+        for row in rows:
+            website_list = row
+    return render_template('user.html', pages=website_list)
+
 
 
 def write(username, password, key):
-    with open('users.csv', 'a') as csvfile:
-        fieldnames = ['username', 'password', 'key']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writerow({'username': username, 'password': hash_password(password), 'key': key})
+    with open('users.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow([username, password, key])
 
 
 def check_user(username):
-    with open('users.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
+    with open('users.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         for row in reader:
-            if username in row['username']:
+            if username in row[0]:
                 return(True)
 
 
 def check_user_pass(username, password):
-    with open('users.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
-        rows = [row for row in reader if row['username'] == username]
+    with open('users.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        rows = [row for row in reader if row[0] == username]
         for row in rows:
-            if check_password(row['password'], password):
+            if check_password(row[1], password):
                 return(True)
             else:
                 return(False)
 
 
 def get_user_key(username, password):
-    with open('users.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
-        rows = [row for row in reader if row['username'] == username]
+    with open('users.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        rows = [row for row in reader if row[0] == username]
         for row in rows:
-            return(row['key'])
+            return(row[2])
 
 
 def hash_password(password):
@@ -104,4 +115,5 @@ def check_password(db_password, user_password):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.secret_key = os.urandom(12)
+    app.run(debug=True)
