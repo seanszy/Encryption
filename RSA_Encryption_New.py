a9881886a4781661c2d9528c3d8392b232e29a29
Chris Aring <christopher.aring@students.olin.edu>
2017-05-03 14:41:58 -0400

    Added comments and removed print

--- RSA_Encryption_New.py
+++ RSA_Encryption_New.py
@@ -9,7 +9,7 @@ def msg_to_int(msg):
 
     msg_int = []
 
-    #Loops through each char"""
+    #Loops through each char
     for char in msg:
         msg_int.append(ord(char) + 100)
     msg_int = int(''.join(map(str, msg_int))) #combines list to integer
@@ -32,8 +32,10 @@ def find_e(phi):
     """The e value is used in RSA encryption. This functions finds the value.
     e is a number that is coprime with phi_q*phi_p"""
 
+    # Generate Primes
     non_primes = set(j for i in range(2, 8) for j in range(i*2, 200, i))
     primes = [x for x in range(2, 200) if x not in non_primes]
+    # Check if prime
     for prime in primes:
         if math.gcd(phi, prime) is 1:
             return(prime)
@@ -53,51 +55,40 @@ def EEA(a, b):
 # ex-phi*q = 1
 def mod_inv(e, phi):
     g, x, y = EEA(e, phi)
-    if g != 1:
-        print('No modular inverse.')
-    else:
+    if g = 1:
         d = (x % phi)
         return(d)
 
 def decrypt(p, q, message):
+    """This function decrypts the user-stored passwords. It takes the stored int representation of their saved password
+    and converts it to a string and returns it."""
     n = p * q
     phi = (p - 1) * (q - 1)
-    #msg = input("Enter Message:")
     msg = int(message)
     e = find_e(phi)
-    print(e)
     d = mod_inv(e, phi)
     decrypted = pow(msg, d, n)
-    print(decrypted)
-    msg_decrypted = int_to_msg(decrypted)
-    print("Your message was:", msg_decrypted)
+    msg_decrypted = int_to_msg(decrypted) #Convert int to string
     return(msg_decrypted)
 
 def encrypt(p, q, message):
+    """This function encrypts a password that a user wishes to store. It converts the string to an int and then excrypts it."""
     n = p * q
     phi = (p - 1) * (q - 1)
-    #msg = input("Enter Message:")
     msg = message
-    msg_int = msg_to_int(msg)
+    msg_int = msg_to_int(msg) #Convert message to int
     e = find_e(phi)
-    print(e)
     encrypted = pow(msg_int, e, n)
     return(encrypted)
 
 def main(p, q, message):
+    """The main function for this process. It handles calling other functions to encrypt and decrypt as needed."""
     n = p * q
     phi = (p - 1) * (q - 1)
-    #msg = input("Enter Message:")
     msg = message
-    msg_int = msg_to_int(msg)
+    msg_int = msg_to_int(msg) #Convert message to int
     e = find_e(phi)
-    print(e)
     d = mod_inv(e, phi)
-    print(204**7)
-    print(14703176545910784%(11*13))
-    encrypted = pow(msg_int, e, n)
-    print("Encrypted Message is:", encrypted)
-    decrypted = pow(encrypted, d, n)
-    print(decrypted)
+    encrypted = pow(msg_int, e, n) #Formula to encrypt
+    decrypted = pow(encrypted, d, n) #Formula to decrypt
     msg_decrypted = int_to_msg(decrypted)
-    print("Your message was:", msg_decrypted)
